46137d8a6a0cb60bdc98f9db4eedd060ef58ec44
Anika Naga <anunaga77@gmail.com>
2020-06-08 13:50:43 -0400

    comment

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Anika Naga on 6/8/20.
+//  Copyright © 2020 Anika Naga. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
